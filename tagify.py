3e8cd42b3b1fbb060267e8d4fc9a613b3471262b
Simon Breiter <hello@simonbreiter.ch>
2016-05-06 21:36:51 +0200

    Refactoring, add argparse

--- tagify.py
+++ tagify.py
@@ -1,41 +1,51 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 import glob
 import re
 import os
+import argparse
 
-files = glob.glob('/Users/simonbreiter/Documents/Notizen/**/*.md')
+parser = argparse.ArgumentParser(description='Parse YAML Front Matters and write according OSX file tags.')
+parser.add_argument('-p', '-path', type=str,
+                    help='Path to folder which contains markdown files.')
 
-tempTag = []
-tagCounter = 0
-counter = 0
+def tagFiles(filePath):
+    tempTag = []
+    tagCounter = 0
+    counter = 0
 
-for file in files:
-    with open(file, "r") as md_file:
-        md_content = md_file.read()
-    yaml_search = re.search('\-\-\-\\n(.*)\\n\-\-\-', md_content)
-    if yaml_search:
-        current_file_with_yaml = {
-            "file": file,
-            "tags": yaml_search.group(1).replace("Tags: ", "").split(',')
-        }
-        filename = current_file_with_yaml['file']
-        tags = current_file_with_yaml['tags']
+    for file in filePath:
+        with open(file, "r") as md_file:
+            md_content = md_file.read()
+        yaml_search = re.search('\-\-\-\\n(.*)\\n\-\-\-', md_content)
+        if yaml_search:
+            current_file_with_yaml = {
+                "file": file,
+                "tags": yaml_search.group(1).replace("Tags: ", "").split(',')
+            }
+            filename = current_file_with_yaml['file']
+            tags = current_file_with_yaml['tags']
 
-        # Add string to tag
-        for tag in tags:
-            tempTag.append("{}{}{}".format("<string>", tag.strip(), "</string>"))
-            tagCounter += 1
+            # Add string to tag
+            for tag in tags:
+                tempTag.append("{}{}{}".format("<string>", tag.strip(), "</string>"))
+                tagCounter += 1
 
-        # combine tags to string
-        taglist = "".join(tempTag)
+            # combine tags to string
+            taglist = "".join(tempTag)
 
-        os.system("xattr -w \'com.apple.metadata:_kMDItemUserTags\' \'<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><array>{0}</array></plist>\' \'{1}\'".format(taglist, filename))
+            os.system("xattr -w \'com.apple.metadata:_kMDItemUserTags\' \'<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><array>{0}</array></plist>\' \'{1}\'".format(taglist, filename))
 
-        # Empty tempTag list
-        tempTag = []
-        counter += 1
+            # Empty tempTag list
+            tempTag = []
+            counter += 1
 
+    os.system("osascript -e \'display notification \"{} Tags von {} Notizen erzeugt\" with title \"Tagify\"\'".format(tagCounter, counter))
 
-os.system("osascript -e \'display notification \"{} Tags von {} Notizen erzeugt\" with title \"Tagify\"\'".format(tagCounter, counter))
+def main():
+    args = parser.parse_args()
+    filePath = glob.glob(args.p + "/**/*.md", recursive=True)
+    tagFiles(filePath)
 
+if __name__ == '__main__':
+    main()
